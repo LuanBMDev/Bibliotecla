bcf0ce33e242341a4e2c7acbf1d42102e7698f43
LuanBMDev <luannbm@gmail.com>
2025-11-12 08:10:26 -0300

    Preparando o Banco com DAO

---
+++ Bibliotecla/Bibliotecla/DAO/DAO.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Bibliotecla.DAO
+{
+    internal interface DAO<T>
+    {
+        void Add(T entity);
+        T GetByID(int id);
+        void Update(T entity);
+        void Delete(T entity);
+        List<T> GetAll();
+    }
+}
