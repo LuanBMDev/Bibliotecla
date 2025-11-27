c0aca4f45b162dab31c675bf365e8b1c2549ea84
LuanBMDev <luannbm@gmail.com>
2025-11-27 17:35:31 -0300

    add: Recolocando a classe de Conexão

---
+++ Bibliotecla/Bibliotecla/Banco/Conexao.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using MySql.Data.MySqlClient;
+
+namespace Bibliotecla.banco
+{
+    internal class Conexao
+    {
+        private static readonly string strConexao =
+            "Server=localhost;Database=bibliotecladb;Uid=root;";
+
+        public static MySqlConnection Conectar()
+        {
+            var conexao = new MySqlConnection(strConexao);
+            conexao.Open();
+            return conexao;
+        }
+
+        public static void Desconectar(MySqlConnection conexao)
+        {
+            if (conexao != null && conexao.State == System.Data.ConnectionState.Open)
+            {
+                conexao.Close();
+            }
+        }
+    }
+}
