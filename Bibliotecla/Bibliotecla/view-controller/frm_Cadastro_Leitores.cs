a15074356a301202d6f1badbca5ed4c3b39f4adc
CarlosDChen <carlosdaniel.chen@gmail.com>
2025-11-29 21:41:39 -0300

    checkpoint
    ate entender o que preciso corrigir

--- Bibliotecla/Bibliotecla/view-controller/frm_Cadastro_Leitores.cs
+++ Bibliotecla/Bibliotecla/view-controller/frm_Cadastro_Leitores.cs
@@ -1,4 +1,6 @@
-﻿using System;
+﻿using Bibliotecla.DAO;
+using Bibliotecla.model;
+using System;
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
@@ -17,6 +19,16 @@ namespace Bibliotecla
             InitializeComponent();
         }
 
+        public bool VerificaCampos()
+        {
+            if (txt_Nome.Text == "" || txt_Cpf.Text == "" || txt_Telefone.Text == "" || txt_Email.Text == "" || txt_Endereco.Text == "")
+            {
+                MessageBox.Show("Por favor, preencha todos os campos obrigatórios.", "Campos Incompletos", MessageBoxButtons.OK, MessageBoxIcon.Warning);
+                return false;
+            }
+            return true;
+        }
+
         private void btn_Voltar_Click(object sender, EventArgs e)
         {
             // 1. Cria uma instância do novo formulário.
@@ -31,6 +43,40 @@ namespace Bibliotecla
 
         private void btn_Cadastrar_Click(object sender, EventArgs e)
         {
+            VerificaCampos();
+
+            string cpf = txt_Cpf.Text.Trim;
+            string nome = txt_Nome.Text.Trim;
+            string endereco = txt_Endereco.Text.Trim;
+            string email = txt_Email.Text.Trim;
+            string telefone = txt_Telefone.Text.Trim;
+            string cargo = "Leitor";
+            string usuario = "";
+            string senha = "";
+            string cep = "";
+            string rua = "";
+            string numRes = "";
+            string cidade = "";
+            string bairro = "";
+            int isDevedor = 0;
+
+            try
+            {
+                LeitorFuncio ObjLeitor = new LeitorFuncio(cpf, telefone, nome, cargo, usuario, senha, cep, endereco, email);
+                LeitorFuncioDAO.Inserir(ObjLeitor);
+
+                MensagensPadrao.msgCadastroSucesso(MensagensPadrao.Entidade.Leitor);
+
+                txt_Cpf.Clear();
+                txt_Nome.Clear();   
+                txt_Endereco.Clear();
+                txt_Email.Clear();
+                txt_Telefone.Clear();
+            }
+            catch (Exception ex)
+            {
+                MensagensPadrao.msgFalhaCadastro(MensagensPadrao.Entidade.Leitor, ex);
+            }
 
         }
     }
